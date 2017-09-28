babebb5e47c747bc83a5912ffaa3debac30ced9d
NghiepVo <vokenghiep@gmail.com>
2017-09-28 18:11:15 +0700

    ok

---
+++ js/components/dashboard/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { AppRegistry, View, StatusBar } from "react-native";
+import {
+  Button,
+  Text,
+  Container,
+  Card,
+  CardItem,
+  Body,
+  Content,
+  Header,
+  Left,
+  Right,
+  Icon,
+  Title,
+  Input,
+  InputGroup,
+  Item,
+  Tab,
+  Tabs,
+  Footer,
+  FooterTab,
+  Label
+} from "native-base";
+
+export default class Dashboard extends React.Component {
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <Container>     
+        <Header>
+          <Left />           
+          <Body>
+            <Title>Dashboard</Title>
+          </Body>
+          
+          <Right />
+        </Header>   
+        <Content padder>
+          <Item floatingLabel style={{ marginTop: 20 }}>
+            <Label>Lucy Chat</Label>
+            <Input />
+          </Item>          
+        </Content>
+      </Container>
+    );
+  }
+}
